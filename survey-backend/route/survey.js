f728c98de748ec953f31b8f7e030d8857d29dfd5
Sreerag <sreerag.as@gmail.com>
2018-10-15 15:28:02 +0530

    API for unmapping questions

--- survey-backend/route/survey.js
+++ survey-backend/route/survey.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const Survey = require("../model/survey");
 const Question = require("../model/question");
 const Result = require("../model/result");
+var inArray = require("in-array");
 
 router.get("/test_survey", (req, res) => {
     res.send("router tested.");
@@ -30,17 +31,17 @@ router.post("/survey", (req, res, next) => {
         if (err) {
             rsltarr = {
                 "status": false,
-                "msg":err.message
+                "msg": err.message
             };
             res.json(rsltarr);
         }
         else {
             rsltarr = {
                 "status": true,
-                "msg":"Survey saved success fullly!!"
+                "msg": "Survey saved success fullly!!"
             };
             res.json(rsltarr);
-           
+
         }
     })
 });
@@ -201,8 +202,21 @@ router.get("/survey_attended/:personId/:surveyId", (req, res, next) => {
 });
 
 
+//SREERAG 15-10-2018
 //method for updating question list
 //will update question list with adding new questions to the existsing ones in the DB. If the parameters have duplicate question IDs, the API will skip them
+//parameter format
+/* 
+{
+    "questions":
+    [
+            "5bbf4f95141f8a24705203f3",
+            "5bb74891bd85e50630ced8ad",
+            "5bbf4f28141f8a24705203f2"
+            
+            ]
+  } 
+  */
 router.put("/question_map/:survey_id", (req, res, next) => {
 
     var rsltarr = {
@@ -210,7 +224,7 @@ router.put("/question_map/:survey_id", (req, res, next) => {
         "msg": ""
     };
 
-    var questionsArra =[];
+    var questionsArra = [];
 
     Survey.find({ _id: req.params.survey_id }, function (err, query_data) {
         if (err) {
@@ -221,21 +235,19 @@ router.put("/question_map/:survey_id", (req, res, next) => {
             res.json(rsltarr);
         }
         else {
-            if(query_data)
-            {
-                
-                if(query_data[0])
-                {
-                    query_data[0].questions.forEach(function(item_exists) {
+            if (query_data) {
+
+                if (query_data[0]) {
+                    query_data[0].questions.forEach(function (item_exists) {
                         questionsArra.push(item_exists);
                     });
-                    req.body.questions.forEach(function(item) {
-                       
+                    req.body.questions.forEach(function (item) {
+
                         questionsArra.push(item);
-                        
+
                     });
-                    
-                   // For avoiding duplicate values in your questions, use the $addToSet operator.
+
+                    // For avoiding duplicate values in your questions, use the $addToSet operator.
                     Survey.update({ _id: req.params.survey_id }, {
                         $addToSet: {
                             questions: questionsArra
@@ -258,24 +270,126 @@ router.put("/question_map/:survey_id", (req, res, next) => {
                             }
                         }
                     );
-                    
-                   
+
+
                 }
-               
+
             }
-            else 
-            {
+            else {
                 rsltarr = {
                     "status": false,
                     "msg": "Invalid survey ID. No survey exist with the provided ID"
                 };
-                res.json(rsltarr); 
+                res.json(rsltarr);
             }
 
         }
     });
 
-    
+
+});
+
+
+
+function contains(arr, element) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == element) {
+            return true;
+        }
+    }
+    return false;
+}
+//SREERAG 15-10-2018
+//method for removing  questions from survey list
+//parameter format - an array of quetion IDs to remove
+/*
+[
+            "5bbf4f95141f8a24705203f3",
+            "5bb74891bd85e50630ced8ad",
+            "5bbf4f28141f8a24705203f2"
+            
+            ]
+*/
+router.put("/unmap_question/:survey_id", (req, res, next) => {
+
+    var rsltarr = {
+        "status": false,
+        "msg": ""
+    };
+
+
+
+    var questionsArra = [];
+
+    Survey.find({ _id: req.params.survey_id }, function (err, query_data) {
+        if (err) {
+            rsltarr = {
+                "status": false,
+                "msg": err.message
+            };
+            res.json(rsltarr);
+        }
+        else {
+            if (query_data) {
+
+                if (query_data[0]) {
+                    query_data[0].questions.forEach(function (item_exists) {
+                        /* var inArray = require('in-array');
+                        console.log(inArray(['a', 'b', 'c'], 'a'));
+                        //=> true */
+
+                        if (!contains(req.body, item_exists))
+                        //if(exists_chk)
+                        {
+                            questionsArra.push(item_exists);
+                        }
+                        /*  if(!inArray(req.body,item_exists))
+                         {
+                             res.send(item_exists+" not exists");  
+                         } */
+
+                    });
+
+                    // For avoiding duplicate values in your questions, use the $addToSet operator.
+                    Survey.update({ _id: req.params.survey_id }, {
+                        $set: {
+                            questions: questionsArra
+                        }
+                    },
+                        function (err, result) {
+                            if (err) {
+                                rsltarr = {
+                                    "status": false,
+                                    "msg": err.message
+                                };
+                                res.json(rsltarr);
+                            }
+                            else {
+                                rsltarr = {
+                                    "status": true,
+                                    "msg": "Success fully unmapped"
+                                };
+                                res.json(rsltarr);
+                            }
+                        }
+                    );
+
+
+                }
+
+            }
+            else {
+                rsltarr = {
+                    "status": false,
+                    "msg": "Invalid survey ID. No survey exist with the provided ID"
+                };
+                res.json(rsltarr);
+            }
+
+        }
+    });
+
+
 });
 
 module.exports = router;
