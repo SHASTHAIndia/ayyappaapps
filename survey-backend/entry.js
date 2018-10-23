519698adb48d35c546a7f79a6b044869556ed488
Sreerag <sreerag.as@gmail.com>
2018-10-23 10:38:06 +0530

    passport.js

--- survey-backend/entry.js
+++ survey-backend/entry.js
@@ -6,20 +6,27 @@ var bodyparser = require("body-parser");
 var cors = require("cors");
 var morgan = require('morgan');
 var passport = require('passport');
-var config = require('./config/database');
+//var config = require('./config/database');
+
+/* var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser'); */
+
+// [SH] Bring in the Passport config after model is defined
+//require('./config/passport');
+
 
 var app = express();
 
 
 
 //const route=require("./route/routes");
-const route_person=require("./route/person");
-const route_survey=require("./route/survey");
-const route_question=require("./route/question");
-const route_result=require("./route/result");
-const route_admin=require("./route/admin.route");
+const route_person = require("./route/person");
+const route_survey = require("./route/survey");
+const route_question = require("./route/question");
+const route_result = require("./route/result");
+const route_admin = require("./route/admin.route");
 //connectto mongo db
-var connection = mongoose.connect("mongodb://localhost:27017/survey");
+var connection = mongoose.connect("mongodb://localhost:27017/survey", { useNewUrlParser: true });
 //var connection = mongoose.connect(config.database);;
 
 mongoose.connection.on("connected", () => {
@@ -34,16 +41,17 @@ const PORT = 3000;
 
 app.use(cors()); //allow server to request and response from different ports in the same machine. Usedto interact with angular project which runs in another port
 
-app.use(passport.initialize());
+
+//app.use(passport.initialize());
 app.use(bodyparser.json()); //helps to parse json data in the request body
 
 //app.use("/api",route); // any call to api will diverted to routes.js
 
-app.use("/person",route_person);
-app.use("/survey",route_survey);
-app.use("/question",route_question);
-app.use("/result",route_result);
-app.use("/admin",route_admin);
+app.use("/person", route_person);
+app.use("/survey", route_survey);
+app.use("/question", route_question);
+app.use("/result", route_result);
+app.use("/admin", route_admin);
 
 app.get("/", (req, res) => {
     res.send("Node Server has been started from the port - " + PORT);
--- survey-backend/route/question.js
+++ survey-backend/route/question.js
@@ -32,8 +32,8 @@ router.post("/question", (req, res, next) => {
 });
 
 //method for updating entry
-router.put("/question", (req, res, next) => {
-    Question.findOneAndUpdate({ _id: req.body.id }, {
+router.put("/question/:id", (req, res, next) => {
+    Question.findOneAndUpdate({ _id: req.params.id }, {
         $set: {
             question: req.body.question,
             //questionCodeNo: req.body.questionCodeNo,
